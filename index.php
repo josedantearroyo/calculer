e1fbcb4698244455d553c52e8598b125c3d602b6
josedantearroyo <malekframi@gmail.com>
2021-07-22 23:17:16 -0500

    todas las operaciones

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 echo "Calculadora";
 
 function suma($a,$b){
-return $a+$b;
+    return $a+$b;
+}
+
+function resta($a,$b){
+    return $a-$b;
+}
+
+function multiplicacion($a,$b){
+    return $a * $b;
+}
+
+function division($a,$b){
+    return $a/$b;
 }
 ?>
